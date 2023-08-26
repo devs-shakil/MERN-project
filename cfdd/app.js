1e70c10df70f1f2025dc546cc5220d2aff38e85c
shakilwebmaster <mdshakil102036@gmail.com>
2023-08-26 08:51:48 +0600

    contact us section

--- cfdd/app.js
+++ cfdd/app.js
@@ -20,3 +20,39 @@ $(document).ready(function(){
         }]
     });
 });
+
+
+
+// show paragraph
+const desktopParagraph = document.querySelector('.desktop-paragraph');
+const mobileParagraph = document.querySelector('.mobile-paragraph');
+const showMoreButton = document.getElementById('showMoreButton');
+
+function toggleParagraphs() {
+  desktopParagraph.style.display = 'block';
+  mobileParagraph.style.display = 'none';
+  showMoreButton.style.display = 'block';
+}
+
+function showFullParagraph() {
+  mobileParagraph.style.display = 'block';
+  showMoreButton.style.display = 'none';
+  desktopParagraph.style.display = 'block';
+}
+
+showMoreButton.addEventListener('click', showFullParagraph);
+
+// Initial check for responsive mode
+function checkResponsiveMode() {
+if (window.innerWidth <= 768) {
+toggleParagraphs();
+} else {
+desktopParagraph.style.display = 'block'; // Display desktop paragraph for wider screens
+mobileParagraph.style.display = 'none';
+showMoreButton.style.display = 'none'; // Hide the "Show More" button
+}
+}
+
+
+window.addEventListener('resize', checkResponsiveMode);
+checkResponsiveMode(); // Check on initial page load
